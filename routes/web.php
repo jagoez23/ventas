290a2c384a62988b7ca515b989301b193c53250f
jagoez23 <desarrolloagoez23@gmail.com>
2024-05-20 17:37:32 -0500

    comfiguración plantilla_categoria

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
+use App\Http\Controllers\categoriaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('template');
 });
+
+/*Route::get('/panel', function () {
+    return view('panel.index')->name('panel');
+});*/
+
+Route::view('/panel', 'panel.index')->name('panel');
+
+Route::resource('categorias',categoriaController::class);
+
+
 
 Route::get('/login', function () {
     return view('auth.login');
